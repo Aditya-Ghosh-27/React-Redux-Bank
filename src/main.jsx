a6fa542e5111375443361b43af283096b60de5d3
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-04-02 16:12:42 +0530

    connecting our react-app with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
 import store from './store'
 
-store.dispatch({type: "account/deposit", payload: 259})
-console.log(store.getState())
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
+
+// After installing react-redux , the next step is to import provider from it.
+// After importing provider, we can now wrapour entire application in that provider like we do with Context API
