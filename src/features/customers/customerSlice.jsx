24a24e72df6aead1fae7d4e1192fff9cc9d46ae5
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-04-05 21:10:06 +0530

    creating the Customer Slice

--- src/features/customers/customerSlice.jsx
+++ src/features/customers/customerSlice.jsx
@@ -1,40 +1,58 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
     fullName: "",
     nationalID: "",
     createdAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-    switch (action.type) {
-      case "customer/createCustomer":
-        return {
-          ...state,
-          fullName: action.payload.fullName,
-          nationalID: action.payload.fullName,
-          createdAt: action.payload.nationalId,
-        };
-      case "customer/updateCustomer":
-        return { ...state, fullName: action.payload };
-      default:
-        return state;
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer(state, action){
+      state.fullName = action.payload.fullName;
+      state.nationalID = action.payload.fullName;
+      state.createdAt = action.payload.nationalID;
+    },
+    updateCustomer(state, action){
+      state.fullName = action.payload;
     }
-}
+  }
+})
+export const { createCustomer, updateCustomer } = customerSlice.actions;
 
-export function createCustomer(fullName, nationalID) {
-    return {
-      type: "customer/createCustomer",
-      payload: {
-        fullName,
-        nationalID,
-        // We could also update the createdAt inside the reducer function(computing the current state) but that would actually be a side effect. And we shouldn't have side effects inside the reducer function
-        createdAt: new Date().toISOString(),
-      },
-    };
-}
+export default customerSlice.reducer;
+// export default function customerReducer(state = initialStateCustomer, action) {
+//     switch (action.type) {
+//       case "customer/createCustomer":
+//         return {
+//           ...state,
+//           fullName: action.payload.fullName,
+//           nationalID: action.payload.fullName,
+//           createdAt: action.payload.nationalId,
+//         };
+//       case "customer/updateCustomer":
+//         return { ...state, fullName: action.payload };
+//       default:
+//         return state;
+//     }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//     return {
+//       type: "customer/createCustomer",
+//       payload: {
+//         fullName,
+//         nationalID,
+//         // We could also update the createdAt inside the reducer function(computing the current state) but that would actually be a side effect. And we shouldn't have side effects inside the reducer function
+//         createdAt: new Date().toISOString(),
+//       },
+//     };
+// }
   
-export function updateName(fullName) {
-    return {
-      type: "customer/updateName",
-      payload: fullName,
-    };
-}  
+// export function updateName(fullName) {
+//     return {
+//       type: "customer/updateName",
+//       payload: fullName,
+//     };
+// }  
